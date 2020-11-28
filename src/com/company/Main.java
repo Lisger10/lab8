8eddacda05c449b81dbc953022eba310fed3b26e
Gera <Germanlis123@gmail.com>
2020-11-28 10:33:17 +0300

    sec com

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 	String url = "src/grades.xml";
 	GradeParser gradeParser = new GradeParser();
+
 	gradeParser.buildGradeSet(url);
         System.out.println(gradeParser.getGrades());
     }
