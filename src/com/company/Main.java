38b7b460c901031da0cbf61c6afd005bd02d7b1e
Gera <Germanlis123@gmail.com>
2020-11-28 10:29:52 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+	String url = "src/grades.xml";
+	GradeParser gradeParser = new GradeParser();
+	gradeParser.buildGradeSet(url);
+        System.out.println(gradeParser.getGrades());
+    }
+}
